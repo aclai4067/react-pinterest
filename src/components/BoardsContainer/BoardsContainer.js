8e76ca86fcb22a02faabdf6e193b77905c4f1658
Ashley Claiborne <languageisaliquid@gmail.com>
2019-12-16 20:45:52 -0600

    add board works

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import boardData from '../../helpers/data/boardsData';
 import authData from '../../helpers/data/authData';
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 class BoardsContainer extends React.Component {
   static propTypes = {
@@ -14,10 +15,21 @@ class BoardsContainer extends React.Component {
     boards: [],
   }
 
-  componentDidMount() {
+  getBoards = () => {
     boardData.getBoardsByUid(authData.getUid())
       .then((boards) => {
         this.setState({ boards });
+      }).catch((err) => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getBoards();
+  }
+
+  addBoard = (boardObj) => {
+    boardData.newBoard(boardObj)
+      .then(() => {
+        this.getBoards();
       }).catch((err) => console.error(err));
   }
 
@@ -25,8 +37,11 @@ class BoardsContainer extends React.Component {
     const { setSingleBoard } = this.props;
 
     return (
-      <div className="row d-flex flex-wrap justify-content-around">
-        {this.state.boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} />)}
+      <div>
+        <BoardForm addNewBoard={this.addBoard} />
+        <div className="row d-flex flex-wrap justify-content-around">
+          {this.state.boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard} />)}
+        </div>
       </div>
     );
   }
